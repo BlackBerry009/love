233d53cd8617a530a024b9cff42f882e39f0006c
BlackBerry009 <765042894@qq.com>
2022-12-07 23:54:27 +0800

    test style

--- main.js
+++ main.js
@@ -1,61 +1,21 @@
 var globe, scene;
 
-const pageContainer = document.querySelector(".container");
-
-/* SMOOTH SCROLL */
-const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true
-});
-
-scroller.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy(pageContainer, {
-  scrollTop(value) {
-    return arguments.length
-      ? scroller.scrollTo(value, 0, 0)
-      : scroller.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      left: 0,
-      top: 0,
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  },
-  pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-window.addEventListener("load", function () {
-  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
-  let pinWrap = document.querySelector(".pin-wrap");
-  let pinWrapWidth = pinWrap.offsetWidth;
-  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-
-  // Pinning and horizontal scrolling
-
-  gsap.to(".pin-wrap", {
-    scrollTrigger: {
-      scroller: pageContainer, //locomotive-scroll
-      scrub: true,
-      trigger: "#sectionPin",
-      pin: true,
-      // anticipatePin: 1,
-      start: "top top",
-      end: pinWrapWidth,
-    },
-    x: -horizontalScrollLength,
-    ease: "none",
-  });
-
-  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-
-  ScrollTrigger.refresh();
-});
-
 window.addEventListener("load", () => {
   const scene = new Scene();
   scene.init();
+
+  let sections = gsap.utils.toArray(".panel");
+  gsap.to(sections, {
+    xPercent:  window.innerWidth * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#sectionPin",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      end: "+=3500",
+    },
+  });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
